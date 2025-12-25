c4be9c2dc67639a2214094608f36ab5d17365a0a
compot7 <nikita03.05.996@gmail.com>
2025-12-25 11:39:50 +0300

    2

--- telehub/settings.py
+++ telehub/settings.py
@@ -16,9 +16,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-in-production-!@#$%^&*()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
